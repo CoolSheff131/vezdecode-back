0e69f79457b4cdd0ee7d6bbe3b6a0930923a3ad5
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-06-05 15:07:15 +0800

    Задание за 30

--- src/main/java/com/example/back/BackApplication.java
+++ src/main/java/com/example/back/BackApplication.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -26,6 +28,7 @@ public class BackApplication {
 	private static final Logger log = LoggerFactory.getLogger(BackApplication.class);
 
 	private static final List<Artist> artists = new ArrayList<>();
+	private static final Map<Date,Vote> votes = new TreeMap<>();
 	private static Bucket bucket;
 
 	private static final int MAX_AMOUNT_OF_REQUESTS = 5;
@@ -69,8 +72,6 @@ public class BackApplication {
 		return map;
 	}
 
-
-
 	@PostMapping("/votes")
 	ResponseEntity  newEmployee(@RequestBody Vote vote) {
 		if(vote.getPhone() == null || vote.getArtist() == null){
@@ -99,6 +100,8 @@ public class BackApplication {
 			if(Objects.equals(artist.getName(), vote.getArtist())){
 				foundArtist = true;
 				artist.setVotes(artist.getVotes() + 1);
+				Date date = new Date();
+				votes.put(date,vote);
 				break;
 			}
 		}
@@ -121,5 +124,93 @@ public class BackApplication {
 		return ResponseEntity.status(HttpStatus.OK)
 				.headers(responseHeaders)
 				.body("Голос Учтен");
+	}
+
+
+
+	@GetMapping("/votes/stats")
+	@ResponseBody
+	public HashMap<String, List< HashMap<String, Long>>>  getVotesFilter(@RequestParam(required = false) String from,
+											  @RequestParam(required = false) String to,
+											  @RequestParam(required = false) String intervals,
+											  @RequestParam(required = false) String artists) throws ParseException {
+		Date fromDate;
+		if(from == null){
+			fromDate = new Date();
+			for (var key: votes.keySet()) {
+				if(key.before(fromDate)){
+					fromDate = key;
+				}
+			}
+		}else{
+			SimpleDateFormat formatter6 = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
+			fromDate = formatter6.parse(from);
+		}
+
+		Date endDate;
+		if(to == null){
+			endDate = new Date();
+			for (var key: votes.keySet()) {
+				if(key.after(endDate)){
+					endDate = key;
+				}
+			}
+		}else{
+			SimpleDateFormat formatter6 = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
+			endDate = formatter6.parse(to);
+		}
+
+		Calendar calendar1 = Calendar.getInstance();
+		calendar1.setTime(fromDate);
+
+		Calendar calendar2 = Calendar.getInstance();
+		calendar2.setTime(endDate);
+		long milSec = calendar2.getTimeInMillis() - calendar1.getTimeInMillis();
+		if(intervals == null){
+			intervals = "10";
+		}
+		long milSecInPeriod = milSec / Integer.parseInt(intervals);
+
+		if(milSecInPeriod < 1000){
+			milSecInPeriod = 1000;
+		}
+		long runMilSec = calendar1.getTimeInMillis();
+		List<Date> dates = new ArrayList<>();
+		while (runMilSec < calendar2.getTimeInMillis()) {
+			var time = runMilSec + milSecInPeriod;
+			runMilSec += milSecInPeriod;
+			var date = new Date(time);
+			dates.add(date);
+		}
+		HashMap<String, List< HashMap<String, Long>>> data = new HashMap<>();
+
+		List<HashMap<String, Long>> dataItems  = new ArrayList<>();
+		List<String> artistArrayFilter = null;
+		if(artists != null){
+			artistArrayFilter = Arrays.stream(artists.split(",")).toList();
+		}
+
+		for (int i = 0; i < dates.size() - 1; i++) {
+			HashMap<String, Long> dataItem = new HashMap<>();
+			calendar1.setTime(dates.get(i));
+			dataItem.put("start", calendar1.getTimeInMillis());
+			calendar1.setTime(dates.get(i + 1));
+			dataItem.put("end", calendar1.getTimeInMillis());
+			var countVotes = 0;
+			for (var key : votes.keySet()) {
+				var artistVote = votes.get(key).getArtist();
+				if(artistArrayFilter != null && !artistArrayFilter.contains(artistVote)){
+					continue;
+				}
+				if (key.after(dates.get(i)) && key.before(dates.get(i+1))) {
+					countVotes += 1;
+				}
+			}
+			dataItem.put("votes", (long) countVotes);
+			dataItems.add(dataItem);
+		}
+
+		data.put("data", dataItems);
+		return data;
 	}
 }
